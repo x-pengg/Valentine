a51e703483b6497a49c1c2dfa0872b24eae16029
x-pengg <i@ridog.me>
2016-08-04 20:55:54 +0800

    New changelist

--- app/src/main/resources/vblog.sql
+++ app/src/main/resources/vblog.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2016-07-29 15:48:13
+Date: 2016-08-04 20:54:37
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS `valentine_comments`;
 CREATE TABLE `valentine_comments` (
   `coid` int(10) NOT NULL AUTO_INCREMENT COMMENT 'comment表主键',
   `cid` int(10) NOT NULL COMMENT 'post表主键,关联字段',
-  `created` int(10) DEFAULT NULL COMMENT '评论生成时的GMT unix时间戳',
+  `created` datetime DEFAULT NULL COMMENT '评论生成时的GMT unix时间戳',
   `author` varchar(200) DEFAULT NULL COMMENT '评论作者',
   `mail` varchar(200) DEFAULT NULL COMMENT '评论者网址',
   `url` varchar(200) DEFAULT NULL COMMENT '评论者网址',
@@ -43,15 +43,15 @@ CREATE TABLE `valentine_contents` (
   `cid` int(10) NOT NULL AUTO_INCREMENT COMMENT 'post表主键',
   `title` varchar(200) DEFAULT NULL COMMENT '内容标题',
   `slug` varchar(200) DEFAULT NULL COMMENT '内容缩略名',
-  `created` int(10) DEFAULT NULL COMMENT '内容生成时的GMT unix时间戳',
-  `modified` int(10) DEFAULT NULL COMMENT '内容更改时的GMT unix时间戳',
+  `created` datetime DEFAULT NULL COMMENT '内容生成时的GMT unix时间戳',
+  `modified` datetime DEFAULT NULL COMMENT '内容更改时的GMT unix时间戳',
   `text` text COMMENT '内容文字',
   `authorId` int(10) NOT NULL COMMENT '内容所属用户id',
-  `status` char(1) DEFAULT '1' COMMENT '内容状态',
-  `allowComment` char(1) DEFAULT '2' COMMENT '是否允许评论',
+  `status` int(1) DEFAULT '1' COMMENT '内容状态',
+  `allowComment` int(1) DEFAULT '2' COMMENT '是否允许评论',
   PRIMARY KEY (`cid`),
   KEY `post_slug` (`slug`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for valentine_metas
@@ -63,10 +63,10 @@ CREATE TABLE `valentine_metas` (
   `slug` varchar(200) DEFAULT NULL COMMENT '项目缩略名',
   `type` varchar(32) DEFAULT NULL COMMENT '项目类型',
   `description` varchar(200) DEFAULT NULL COMMENT '选项描述',
-  `count` int(10) DEFAULT NULL COMMENT '项目所属内容个数',
+  `count` int(10) DEFAULT '0' COMMENT '项目所属内容个数',
   PRIMARY KEY (`mid`),
   KEY `metas_slug` (`slug`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for valentine_relationships
@@ -87,8 +87,8 @@ CREATE TABLE `valentine_users` (
   `name` varchar(32) DEFAULT NULL COMMENT '用户名称',
   `password` varchar(32) DEFAULT NULL COMMENT '用户密码',
   `mail` varchar(200) DEFAULT NULL COMMENT '用户的邮箱',
-  `created` bigint(10) DEFAULT NULL COMMENT '用户注册时的GMT unix时间戳',
-  `activated` bigint(10) DEFAULT NULL COMMENT '最后活动时间',
+  `created` datetime NOT NULL COMMENT '用户注册时的GMT unix时间戳',
+  `activated` datetime NOT NULL COMMENT '最后活动时间',
   PRIMARY KEY (`uid`),
   UNIQUE KEY `users_name` (`name`) USING BTREE,
   UNIQUE KEY `users_mail` (`mail`)
